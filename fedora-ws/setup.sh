67fdeda1202f4bc2b9dd57506841f3cfac0ca6e7
Norrick McGee <norrick.mcgee@gmail.com>
2022-11-23 11:31:41 -0600

    Adjusted nvim config handling

--- fedora-ws/setup.sh
+++ fedora-ws/setup.sh
@@ -4,13 +4,10 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 
 sudo dnf install neovim steam -y
 
-#git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-#   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-mkdir ~/.config/nvim
-cp ~/init.lua ~/.config/nvim
+cp ../nvim ~/.config
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 flatpak install com.discordapp.Discord -y
