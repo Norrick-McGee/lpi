8c1d7f6ab705da6acd2ebbec3a82381ca0718920
Norrick McGee <norrick.mcgee@gmail.com>
2022-11-24 20:32:22 -0600

    Cleaned setup script

--- fedora-ws/setup.sh
+++ fedora-ws/setup.sh
@@ -2,7 +2,16 @@
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Non-free sound and video codecs useful for watching youtube
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate sound-and-video
+
 sudo dnf install neovim steam -y
+
+# Enable Docker Daemon
+sudo dnf install docker -y 
+sudo systemctl enable docker
+sudo systemctl start docker
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
